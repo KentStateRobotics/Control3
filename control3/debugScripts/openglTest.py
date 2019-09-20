4d8d2eda8cd7685be5a51a36b2222b5c4de4431a
JaredButcher <jared.butcher1219@gmail.com>
2019-09-20 15:15:40 -0400

    pyopengl is giving me a headache

--- control3/debugScripts/openglTest.py
+++ control3/debugScripts/openglTest.py
@@ -41,33 +41,20 @@ def cube():
 
     GL.glEnd()
 
-def resize(width, height):
-    GL.glViewport(0, 0, width, height)
-    print(width)
-    print(height)
+def resize(width, height, options):
+    pygame.display.set_mode((width, height), options)
     GLU.gluPerspective(45, width / height, 0.1, 50.0)
-
-    #GL.glMatrixMode(GL.GL_PROJECTION)
-    #GL.glLoadIdentity()
-    #GL.glOrtho(0, width, height, 0, .1, 50)
-
     GL.glTranslatef(0.0, 0.0, -5)
-
-    GL.glMatrixMode(GL.GL_MODELVIEW)
-    GL.glLoadIdentity()
+    GL.glEnable(GL.GL_CULL_FACE);
+    GL.glCullFace(GL.GL_FRONT)
+    GL.glEnable(GL.GL_DEPTH_TEST)
     
 
 def main():
     pygame.init()
     displayOptions = pygame.DOUBLEBUF|pygame.OPENGL|pygame.RESIZABLE|pygame.HWSURFACE
-    pygame.display.set_mode((800,600), displayOptions)
 
-    #resize(800, 600)
-    GLU.gluPerspective(45, 800 / 600, 0.1, 50.0)
-    GL.glEnable(GL.GL_CULL_FACE);
-    GL.glCullFace(GL.GL_FRONT)
-    GL.glEnable(GL.GL_DEPTH_TEST)
-    GL.glTranslatef(0.0, 0.0, -5)
+    resize(800, 600, displayOptions)
 
     while True:
         for event in pygame.event.get():
@@ -75,10 +62,8 @@ def main():
                 print("Closing")
                 pygame.quit()
             elif event.type == pygame.VIDEORESIZE:
-                #screen = pygame.display.set_mode(event.size, displayOptions)
-                print(event.w)
-                #GLU.gluPerspective(45, event.w / event.h, 0.1, 50.0)
-                #resize(event.w, event.h)
+                print(event.size)
+                resize(event.w, event.h, displayOptions)
     
         GL.glRotatef(1.5,1.5,1.5,1)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
