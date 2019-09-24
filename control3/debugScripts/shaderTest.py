75359efdc22db4ceabf96b18329885672811053a
JaredButcher <jared.butcher1219@gmail.com>
2019-09-23 23:23:57 -0400

    Shader test printing square

--- control3/debugScripts/shaderTest.py
+++ control3/debugScripts/shaderTest.py
@@ -31,7 +31,7 @@ triangles = np.array([
     (7,4,5),
     (4,1,5),
     (1,7,5)
-], dtype=np.int32)
+], dtype=np.uint16)
 triBuffer = vbo.VBO(triangles, target=gl.GL_ELEMENT_ARRAY_BUFFER)
 
 
@@ -39,7 +39,7 @@ def resize(width, height, options):
     pygame.display.set_mode((width, height), options)
     glu.gluPerspective(45, width / height, 0.1, 50.0)
     gl.glTranslatef(0.0, 0.0, -5)
-    gl.glEnable(gl.GL_CULL_FACE);
+    gl.glEnable(gl.GL_CULL_FACE)
     gl.glEnable(gl.GL_DEPTH_TEST)
     
 
@@ -54,21 +54,31 @@ def main():
     gl.glClearColor (0.0, 0.5, 0.5, 1.0)
     gl.glEnableClientState (gl.GL_VERTEX_ARRAY)
 
-    vertices = [ 0.0, 1.0, 0.0,  0.0, 0.0, 0.0,  1.0, 1.0, 0.0 ]
-    vbo = gl.glGenBuffers (1)
-    gl.glBindBuffer (gl.GL_ARRAY_BUFFER, vbo)
-    gl.glBufferData (gl.GL_ARRAY_BUFFER, len(vertices)*4, (ctypes.c_float*len(vertices))(*vertices), gl.GL_STATIC_DRAW)
+    print(len(verticies))
+    print(len(triangles))
 
+    vbo = gl.glGenBuffers(1)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, len(verticies) * 4 * 3, verticies, gl.GL_STATIC_DRAW)
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+    gl.glEnableVertexAttribArray(0)
+
+    indiceVbo = gl.glGenBuffers(1)
+    gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indiceVbo)
+    gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, len(triangles) * 2 * 3, triangles, gl.GL_STATIC_DRAW)
 
     VERTEX_SHADER = shaders.compileShader("""
+        #version 400
+        layout(location=0) in vec3 in_Position;
         void main() {
-            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            gl_Position = vec4(in_Position, 1);
         }""", gl.GL_VERTEX_SHADER)
 
 
     FRAGMENT_SHADER = shaders.compileShader("""
+        #version 400
         void main() {
-            gl_FragColor = vec4( 0, 1, 0, 1 );
+            gl_FragColor = vec4( 1, 0, 0, 1 );
         }""", gl.GL_FRAGMENT_SHADER)
 
     shader = shaders.compileProgram(VERTEX_SHADER,FRAGMENT_SHADER)
@@ -81,36 +91,12 @@ def main():
             elif event.type == pygame.VIDEORESIZE:
                 print(event.size)
                 #resize(event.w, event.h, displayOptions)
-        '''
-        #gl.glRotatef(1.5,1.5,1.5,1)
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-
+        gl.glClear (gl.GL_COLOR_BUFFER_BIT)
+        
         gl.glUseProgram(shader)
-        try:
-            stuff.bind()
-            try:
-                gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-                gl.glDrawArrays(gl.GL_TRIANGLES, 0, 9)
-            finally:
-                stuff.unbind()
-                gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
-        finally:
-            gl.glUseProgram( 0 )
-
-
-        pygame.display.flip()
-        pygame.time.wait(10)'''
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-        gl.glUseProgram(shader)
-        #gl.glBindBuffer (gl.GL_ARRAY_BUFFER, vbo)
-        #gl.glVertexPointer (3, gl.GL_FLOAT, 0, None)
-        #gl.glDrawArrays (gl.GL_TRIANGLES, 0, 3)
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertBuffer)
-        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, triBuffer)
-        gl.glEnableVertexAttribArray(0)
-        gl.glVertexAttribPointer(0, 6, gl.GL_FLOAT, False, 0, None)
-        gl.glDrawElements(gl.GL_TRIANGLES, 12, gl.GL_UNSIGNED_INT, None)
-        gl.glUseProgram(0)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indiceVbo)
+        gl.glDrawElements(gl.GL_TRIANGLES, 12, gl.GL_UNSIGNED_SHORT, None)
 
         pygame.display.flip()
         pygame.time.wait(10)
