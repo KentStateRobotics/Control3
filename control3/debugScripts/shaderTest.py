99d96cf43dae54819498ab8db062da6ff427643e
Jared Butcher <jared.butcher1219@gmail.com>
2019-09-27 12:33:23 -0400

    more shader testing

--- control3/debugScripts/shaderTest.py
+++ control3/debugScripts/shaderTest.py
@@ -39,23 +39,10 @@ def resize(width, height, options):
     pygame.display.set_mode((width, height), options)
     glu.gluPerspective(45, width / height, 0.1, 50.0)
     gl.glTranslatef(0.0, 0.0, -5)
-    gl.glEnable(gl.GL_CULL_FACE)
-    gl.glEnable(gl.GL_DEPTH_TEST)
-    
-
-def main():
-    pygame.init()
-    displayOptions = pygame.DOUBLEBUF|pygame.OPENGL|pygame.RESIZABLE|pygame.HWSURFACE
-
-    #resize(800, 600, displayOptions)
-
-    screen = pygame.display.set_mode ((800,600), pygame.OPENGL|pygame.DOUBLEBUF, 24)
-    gl.glViewport (0, 0, 800, 600)
+    #gl.glEnable(gl.GL_CULL_FACE)
+    #gl.glEnable(gl.GL_DEPTH_TEST)
     gl.glClearColor (0.0, 0.5, 0.5, 1.0)
     gl.glEnableClientState (gl.GL_VERTEX_ARRAY)
-
-    print(len(verticies))
-    print(len(triangles))
 
     vbo = gl.glGenBuffers(1)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
@@ -68,20 +55,35 @@ def main():
     gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, len(triangles) * 2 * 3, triangles, gl.GL_STATIC_DRAW)
 
     VERTEX_SHADER = shaders.compileShader("""
-        #version 400
+        #version 430
         layout(location=0) in vec3 in_Position;
+        uniform mat4 MVP;
         void main() {
-            gl_Position = vec4(in_Position, 1);
+            gl_Position = MVP * vec4(in_Position, 1);
         }""", gl.GL_VERTEX_SHADER)
 
 
     FRAGMENT_SHADER = shaders.compileShader("""
-        #version 400
+        #version 430
         void main() {
             gl_FragColor = vec4( 1, 0, 0, 1 );
         }""", gl.GL_FRAGMENT_SHADER)
 
-    shader = shaders.compileProgram(VERTEX_SHADER,FRAGMENT_SHADER)
+    print(gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
+
+    return shaders.compileProgram(VERTEX_SHADER,FRAGMENT_SHADER)
+    
+
+
+def main():
+    pygame.init()
+    displayOptions = pygame.DOUBLEBUF|pygame.OPENGL|pygame.RESIZABLE|pygame.HWSURFACE
+
+    shader = resize(800, 600, displayOptions)
+
+    print(gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
+
+    angle = 0
 
     while True:
         for event in pygame.event.get():
@@ -90,13 +92,34 @@ def main():
                 pygame.quit()
             elif event.type == pygame.VIDEORESIZE:
                 print(event.size)
-                #resize(event.w, event.h, displayOptions)
+                shader = resize(event.w, event.h, displayOptions)
         gl.glClear (gl.GL_COLOR_BUFFER_BIT)
-        
+
         gl.glUseProgram(shader)
+        MVP = gl.glGetUniformLocation(shader, 'MVP')
+        gl.glUniformMatrix4fv(MVP, 1, gl.GL_FALSE, gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
+
+        vbo = gl.glGenBuffers(1)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+        gl.glBufferData(gl.GL_ARRAY_BUFFER, len(verticies) * 4 * 3, verticies, gl.GL_STATIC_DRAW)
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, None)
+        gl.glEnableVertexAttribArray(0)
+
+        indiceVbo = gl.glGenBuffers(1)
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indiceVbo)
+        gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, len(triangles) * 2 * 3, triangles, gl.GL_STATIC_DRAW)
+
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
         gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indiceVbo)
-        gl.glDrawElements(gl.GL_TRIANGLES, 12, gl.GL_UNSIGNED_SHORT, None)
+        gl.glDrawElements(gl.GL_TRIANGLES, 12 * 3, gl.GL_UNSIGNED_SHORT, None)
+
+        gl.glPushMatrix()
+        gl.glTranslatef(-1.5, 0, 0)
+        angle += 1
+        gl.glRotatef(angle, 1, 0, 0)
+        gl.glUniformMatrix4fv(MVP, 1, gl.GL_FALSE, gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
+        gl.glDrawElements(gl.GL_TRIANGLES, 12 * 3, gl.GL_UNSIGNED_SHORT, None)
+        gl.glPopMatrix()
 
         pygame.display.flip()
         pygame.time.wait(10)
