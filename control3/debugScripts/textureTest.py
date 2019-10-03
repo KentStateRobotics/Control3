ee4d60b541835618733c699d3525715f8a5765b2
Jared Butcher <jared.butcher1219@gmail.com>
2019-10-03 16:38:28 -0400

    text writing test

--- control3/debugScripts/textureTest.py
+++ control3/debugScripts/textureTest.py
@@ -5,15 +5,17 @@ from OpenGL.GL import shaders
 from OpenGL.arrays import vbo
 import numpy as np
 import ctypes
+import cv2
+
 
 verticies = np.array([
     (0,0,0,0,0),
-    (0,0,1,0,1),
-    (0,1,0,1,0),
-    (0,1,1,1,1),
-    (1,0,0,1,0),
-    (1,0,1,0,1),
-    (1,1,0,1,1),
+    (0,0,1,0,0),
+    (0,1,0,0,0),
+    (0,1,1,0,0),
+    (1,0,0,0,0),
+    (1,0,1,0,0),
+    (1,1,0,0,0),
     (1,1,1,0,0)
 ], dtype=np.float32)
 vertBuffer = vbo.VBO(verticies)
@@ -39,7 +41,7 @@ def resize(width, height, options):
     pygame.display.set_mode((width, height), options)
     glu.gluPerspective(45, width / height, 0.1, 50.0)
     gl.glTranslatef(0.0, 0.0, -5)
-    #gl.glEnable(gl.GL_CULL_FACE)
+    gl.glEnable(gl.GL_CULL_FACE)
     #gl.glEnable(gl.GL_DEPTH_TEST)
     gl.glEnable(gl.GL_TEXTURE_2D)
     gl.glClearColor (0.0, 0.5, 0.5, 1.0)
@@ -49,17 +51,27 @@ def resize(width, height, options):
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
     gl.glBufferData(gl.GL_ARRAY_BUFFER, len(verticies) * 4 * 5, verticies, gl.GL_STATIC_DRAW)
     gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 5 * 4, None)
-    gl.glVertexAttribPointer(1, 2, gl.GL_FLOAT, gl.GL_FALSE, 5 * 4, 3 * 4)
+    gl.glVertexAttribPointer(1, 2, gl.GL_FLOAT, gl.GL_FALSE, 5 * 4, None)
     gl.glEnableVertexAttribArray(0)
     gl.glEnableVertexAttribArray(1)
 
-    textureImg = pygame.image.load("testTexture1.jpg")
-    textureData = pygame.image.tostring(textureImg, "RGBA", 1)
+    textureImg = pygame.font.Font(None, 50).render("Hello", True, (255, 0, 0), (0,255,0))
+    #textureImg = pygame.image.load("testTexture1.jpg")
+    print(textureImg.get_height())
+    print(textureImg.get_width())
+    textureData = pygame.image.tostring(textureImg, "RGB", 1)
+    npImg = np.fromstring(textureData, dtype=np.uint8)
+    npImg = np.reshape(npImg, (textureImg.get_height(), textureImg.get_width(), 3))
+    cv2.imshow("hello", npImg)
+    cv2.waitKey(0)
     textureBuffer = gl.glGenTextures(1)
+    #gl.glActiveTexture(gl.GL_TEXTURE0)
+    #gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT,1)
     gl.glBindTexture(gl.GL_TEXTURE_2D, textureBuffer)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, 4, textureImg.get_width(), textureImg.get_height(), 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, textureData)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, 4, textureImg.get_width(), textureImg.get_height(), 
+    0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, textureData)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
 
 
     indiceVbo = gl.glGenBuffers(1)
@@ -85,10 +97,9 @@ def resize(width, height, options):
         uniform sampler2D myTexture; 
         void main() {
             color = texture(myTexture, UV).rgba;
-            //color = vec3(1,0,0,1);
+            //color = vec3(1,UV[0],UV[1]);
         }""", gl.GL_FRAGMENT_SHADER)
 
-    print(gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
 
     return shaders.compileProgram(VERTEX_SHADER,FRAGMENT_SHADER)
     
@@ -99,8 +110,6 @@ def main():
     displayOptions = pygame.DOUBLEBUF|pygame.OPENGL|pygame.RESIZABLE|pygame.HWSURFACE
 
     shader = resize(800, 600, displayOptions)
-
-    print(gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX))
 
     angle = 0
 
