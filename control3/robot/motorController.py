9604939574519452f7918bc2445e28c1864a0ddf
JaredButcher <jared.butcher1219@gmail.com>
2020-02-04 20:25:21 -0500

    more motor controller stuff

--- control3/robot/motorController.py
+++ control3/robot/motorController.py
@@ -1,4 +1,5 @@
 import struct
+import time
 
 #big edian 2 chars
 motorStruct = struct.Struct("!cc")
@@ -9,30 +10,50 @@ class MotorController:
     Args:
         motorId (byte): single byte to identify this on the serial connection
         hasFeedback (bool, Optional): Does this motor have a pulse feedback
-        signed (bool, Optional): Can this motor move backwards, will it accept negitive move commands
     """
-    def __init__(self, motorId, hasFeedback=False, signed=True):
+    def __init__(self, motorId, hasFeedback=False):
         self.motorId = motorId
         self.hasFeedback = hasFeedback
         self.command = 0
         self.signed = signed
+        self.deltaDisplacement = 0
+        self.oldTime = 0
+        self.position = 0 #Rad angle of device
         #TODO serial stuff to find corisponding motor
+        #TODO register callback with serial stuff to receive motor feedback
     
-    def getCommand():
+    def getCommand(self):
+        """Get last command to motor
+        """
         return self.command
+
+    def getPosition(self):
+        """Returns absolute angle of motor's output
+        """
+        return self.position
+
+    def getDeltaDisplacement(self):
+        """Get time and difference in position from last time this was called
+        Returns: 
+            float, float: change in time, cumulative change in position sense last call 
+        """
+        deltaTime = time.time() - self.oldTime
+        self.oldTime = time.time()
+        return deltaTime, self.deltaDisplacement
+
+    def _receiveFeedback(self, message):
+        """Callback to receive feedback from motor controlers
+        """
+        pass
     
-    def command(value):
+    def command(self, value):
         """Command the motor to move
         Args:
-            value (int): [-1,1] if singed or [0,1] if unsinged. Sends a command.
+            value (int): [-1,1] value to command
         """
         if -1 > value or value > 1:
             raise ValueError("Motor command must be in the range [-1,1]. " + str(value) + " was given.")
-        elif (not self.signed) and value < 0:
-            raise ValueError("Unsinged motor command must be in the range [0,1]. " + str(value) + " was given.")
+            return
         self.command = value
-        if self.signed:
-            value = value * 127 + 127
-        else:
-            value = value * 255
+        value = value * 127 + 127
         #TODO send motorStruct.pack(self.motorId, value))
